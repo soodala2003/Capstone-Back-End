d370be1e15beb32b1a6439f200d74aadb4b6389b
soodala2003 <soodala78@gmail.com>
2025-01-04 16:27:53 -0500

    added files

---
+++ routes/season1.js
@@ -0,0 +1,37 @@
+import express from "express";
+import mongoose from "mongoose";
+import Season1 from "../models/Season1.js";
+
+const router = express.Router();
+
+// Get all episodes of season1
+router.get("/", async (req, res) => {
+    try {
+      const episodes = await Season1.find({});
+      res.status(200).json({ success: true,  data: episodes });
+    } catch (error) {
+      console.log("Error in fetching episodes: ", error.message);
+      res.status(500).json({ success: false, message: "Server Error" });
+    }
+});
+
+// Get a single episode 
+router.get("/:id", async (req, res) => {
+  const {id} = req.params;
+
+  try {
+    const episode = await Season1.findById(id);
+
+    if (!episode) {
+      return res.status(404).json({ success: false, message: "Episode not found" });
+    }
+
+    res.status(200).json({ success: true,  data: episode });
+  } catch (error) {
+    console.log("Error in fetching Episode: ", error.message);
+    res.status(404).json({ success: false, message: "Episode not found" });
+  }
+});
+
+
+export default router;
