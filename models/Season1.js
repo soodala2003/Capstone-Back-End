394221d97e8a7b9906b0764a0aed6e9696554b6a
soodala2003 <soodala78@gmail.com>
2025-01-07 02:57:07 -0500

    updated files

--- models/Season1.js
+++ models/Season1.js
@@ -1,10 +1,7 @@
 import mongoose from "mongoose";
 
-//const ObjectId = mongoose.Types.ObjectId;
-
 const season1Schema = new mongoose.Schema(
   {
-    /* _id: { type: ObjectId }, */
     name: { type: String },
     air_date: { type: String },
     episode: { type: String },
